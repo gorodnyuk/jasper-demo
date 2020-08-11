b5db66b419d26139ba807f7cddec1f96f499eaf1
Sergey Gorodnyuk <sergeygorodnyuk@yandex.ru>
2020-08-11 23:23:21 +0300

    change report to byte array

--- src/main/java/uk/gorodny/jasperreportdemo/JasperController.java
+++ src/main/java/uk/gorodny/jasperreportdemo/JasperController.java
@@ -1,16 +1,13 @@
 package uk.gorodny.jasperreportdemo;
 
 import net.sf.jasperreports.engine.JRException;
-import net.sf.jasperreports.engine.JasperExportManager;
-import net.sf.jasperreports.engine.JasperPrint;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @RestController
 public class JasperController {
@@ -19,13 +16,15 @@ public class JasperController {
     private JasperService jasperService;
 
     @GetMapping("/jasper")
-    public void getReport(@RequestParam("name") String name,
-                          @RequestParam("surname") String surname,
-                          HttpServletResponse httpServletResponse) throws IOException, JRException {
-        JasperPrint jasperPrint = jasperService.generateReport(new Man(name, surname));
-        httpServletResponse.setContentType("application/x-pdf");
-        httpServletResponse.setHeader("Content-disposition", "attachment; filename=report.pdf");
-        OutputStream outputStream = httpServletResponse.getOutputStream();
-        JasperExportManager.exportReportToPdfStream(jasperPrint, outputStream);
+    public ResponseEntity<byte[]> getReport(@RequestParam("name") String name,
+                                            @RequestParam("surname") String surname) throws IOException, JRException {
+        byte[] reportBytes = jasperService.generateReport(new Man(name, surname));
+        return ResponseEntity
+                .ok()
+                // Specify content type as PDF
+                .header("Content-Type", "application/pdf; charset=UTF-8")
+                // Tell browser to display PDF if it can
+                .header("Content-Disposition", "attachment; filename=report.pdf")
+                .body(reportBytes);
     }
 }
--- src/main/java/uk/gorodny/jasperreportdemo/JasperService.java
+++ src/main/java/uk/gorodny/jasperreportdemo/JasperService.java
@@ -13,13 +13,14 @@ import java.util.Map;
 @Service
 public class JasperService {
 
-    public JasperPrint generateReport(Man man) throws JRException, FileNotFoundException {
+    public byte[] generateReport(Man man) throws JRException, FileNotFoundException {
         String filePath = ResourceUtils.getFile("classpath:jasper-demo.jrxml").getAbsolutePath();
         JasperReport jasperReport = JasperCompileManager.compileReport(filePath);
         Map<String, Object> params = new HashMap<>();
         params.put("demoName", man.getName());
         params.put("demoSurname", man.getSurname());
         JRBeanCollectionDataSource jrBeanCollectionDataSource = new JRBeanCollectionDataSource(Collections.singletonList(""));
-        return JasperFillManager.fillReport(jasperReport, params, jrBeanCollectionDataSource);
+        JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, jrBeanCollectionDataSource);
+        return JasperExportManager.exportReportToPdf(jasperPrint);
     }
 }
